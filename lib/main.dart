881772e1c4174c0a07ee608babaf4cf16e3f67d3
廖宇浩 <taiyangniaoming@foxmail.com>
2018-11-01 10:57:52 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("gif"),
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: new Center(
-          child: new Image.asset('gif/plane.gif'),
-        ),
-      )
-    );
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("gif"),
+          ),
+          body: new Row(
+            children: <Widget>[
+              new Image.asset('gif/plane.gif'),
+              new Image.asset('gif/Cool.gif'),
+              new Image.asset('gif/giphyy.gif'),
+              new Image.asset('gif/airplane.gif'),
+            ],
+          ),
+        ));
   }
 }
